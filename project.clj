c6d9484723789a07746bfb3b80c7abb80c437a93
Kenneth Foner <kenny.foner@gmail.com>
2013-05-25 13:03:54 -0400

    No longer requires numeric tower.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject lazy-combinatorics "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
