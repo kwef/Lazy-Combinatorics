7cc720b277b1c2409df7e720e5058f71267782e3
Kenneth Foner <kenny.foner@gmail.com>
2014-04-04 22:11:44 -0400

    Code cleanup

--- Haskell/Cartesian.hs
+++ Haskell/Cartesian.hs
@@ -17,14 +17,16 @@ diagonalize :: [[a]] -> [[a]]
 diagonalize =
    unfoldr (uncurry $ \n ->
       toMaybe (not . null . fst)
-      . second (n + 1,) . stripN n)
+      . second (n + 1,)
+      . stripN n)
    . (1,)
 
 stripN :: Int -> [[a]] -> ([a],[[a]])
 stripN n list =
-   (mapMaybe headMay firstN, mapMaybe tailMay firstN ++ rest)
+   (mapMaybe headMay firstN,
+    mapMaybe tailMay firstN ++ rest)
    where (firstN,rest) = splitAt n list
 
 toMaybe :: (a -> Bool) -> a -> Maybe a
-toMaybe predicate item | predicate item = Just item
-toMaybe _         _    | otherwise      = Nothing
+toMaybe p x | p x = Just x
+toMaybe _ _       = Nothing
