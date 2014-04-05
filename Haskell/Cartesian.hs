ad3f3250b1b718a913eb5a4a75b36ecca2e1360a
Kenneth Foner <kenny.foner@gmail.com>
2014-04-04 21:32:11 -0400

    Fair cartesian product for pairs in Haskell

--- Haskell/Cartesian.hs
+++ Haskell/Cartesian.hs
@@ -1,1 +1,27 @@
+{-# LANGUAGE TupleSections #-}
+
 module Cartesian where
+
+import Data.List
+import Data.Maybe
+import Control.Monad
+import Control.Arrow
+import Safe
+
+diagonalize :: [[a]] -> [[a]]
+diagonalize =
+   unfoldr (uncurry $ \n ->
+      mfilter (not . null . fst) . Just
+      . second (n + 1,) . stripN n)
+   . (1,)
+
+stripN :: Int -> [[a]] -> ([a],[[a]])
+stripN n list =
+   let (xs,rest) = splitAt n list
+   in (mapMaybe headMay xs, mapMaybe tailMay xs ++ rest)
+
+pairSpace :: [a] -> [b] -> [[(a,b)]]
+pairSpace xs ys = map (\x -> map (x,) ys) xs
+
+cartesian :: [a] -> [b] -> [(a,b)]
+cartesian = curry $ concat . diagonalize . uncurry pairSpace
